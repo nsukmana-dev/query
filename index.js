1c73709e2fe11573cd9327ccb39f4aab741d2bcb
Nandar Sukmana <nsukmana.dev@gmail.com>
2022-11-24 14:07:31 +0700

    service query

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+
+const app = express();
+app.use(bodyParser.json());
+
+app.get("/posts", (req, res) => {});
+app.post("/events", (req, res) => {});
+
+app.listen(4002, () => {
+  console.log("Listening on 4002");
+});
